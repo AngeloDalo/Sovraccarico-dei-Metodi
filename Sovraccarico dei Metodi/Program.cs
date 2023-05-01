fa46ba3286c1588bae0e88d19f38a586b60739d4
Angelo <Angelo@DESKTOP-6JKRPIT>
2023-05-01 09:39:04 +0200

    Aggiungere i file di progetto.

---
+++ Sovraccarico dei Metodi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Corso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int risultato = FaiSomma(10, 20);
+            int risultato2 = FaiSomma(10, 20, 30);
+        }
+
+        static int FaiSomma(int num1, int num2)
+        {
+            return num1+ num2;
+        }
+        static int FaiSomma(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+}
